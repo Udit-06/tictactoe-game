157713554f18a503fd92fd325497c90c20e409ca
Udit-06 <Uditanshu.burra2000@gmail.com>
2023-07-20 11:07:45 +0530

    made no-unused-vars produce a warning

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,26 @@
-import './styles.scss'
-import Board from './components/Board'
+import { useState } from 'react';
+import './styles.scss';
+import Board from './components/Board';
 
 function App() {
+  const [counter, setCounter] = useState(1);
+
+  console.log('hello');
+
+  const onBtnClick = () => {
+    setCounter((currentCounter) => {
+      return currentCounter + 1
+    });
+  };
 
   return (
-  <div className='app'>
-    <Board />
-  </div>
-  )
+    <div className="app">
+      <div>
+        <button onClick={onBtnClick}>Click me please</button>
+        <div>{counter}</div>
+      </div>
+    </div>
+  );
 }
 
-export default App
+export default App;
