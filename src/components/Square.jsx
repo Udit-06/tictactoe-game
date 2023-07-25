4d0e55610f50453b349f23bfbe2fbf142c31950d
Udit-06 <Uditanshu.burra2000@gmail.com>
2023-07-25 10:41:43 +0530

    highlighted winning combination

--- src/components/Square.jsx
+++ src/components/Square.jsx
@@ -1,6 +1,13 @@
-const Square = ({ value, onClick }) => {
+const Square = ({ value, onClick, iswinningSquare }) => {
+  const colorClassName = value === 'X' ? 'text-green' : 'text-orange';
+  const winningClassName = iswinningSquare ? 'winning' : '';
+
   return (
-    <button type="button" className="square" onClick={onClick}>
+    <button
+      type="button"
+      className={`square ${colorClassName} ${winningClassName}`}
+      onClick={onClick}
+    >
       {value}
     </button>
   );
